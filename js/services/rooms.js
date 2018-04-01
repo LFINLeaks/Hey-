4f6209f69fb71af4bd6d43ea746d63fe7057f847
LFINLeaks <37758337+LFINLeaks@users.noreply.github.com>
2018-04-01 15:30:06 -0400

    h

--- js/services/rooms.js
+++ js/services/rooms.js
@@ -3,138 +3,45 @@ app.factory('Articles', function ($http){
   var Articles = {
     'articles' : [
       {
-        "title" : 'Silicon Valley',
+        "title" : 'Longfin CEO Venkat S. Meenavalli on CNBC',
         "icon" : "http://www.returndates.com/backgrounds/siliconvalley.logo.png",
-        "banner" : "./img/sv/cover.jpg",
+        "banner" : "./img/cnbc/cover.svg",
         "bgcolor": "#62C6F4",
         "sounds" : [
-          {
-            "title" : "This is Pied Piper",
-            "url" : "./sounds/sv/this-pied-piper.wav",
-            "background" : "./img/sv/cover.jpg"
-          },
-          {
-            "title" : "Eat The Fish",
-            "url" : "./sounds/sv/eat-the-fish.wav",
-            "background" : "./img/sv/cover.jpg"
-          },
-          {
-            "title" : "Tell something",
-            "url" : "./sounds/sv/tell-something.wav",
-            "background" : "./img/sv/cover.jpg"
-          },
-
-          {
-            "title" : "Aviato !",
-            "url" : "./sounds/sv/aviato.mp3",
-            "background" : "./img/sv/cover.jpg"
-          },
-
-          {
-            "title" : "Clowns",
-            "url" : "./sounds/sv/clowns.wav",
-            "background" : "./img/sv/cover.jpg"
-          },
-
-          {
-            "title" : "Let's Pivot",
-            "url" : "./sounds/sv/lets-pivot.mp3",
-            "background" : "./img/sv/cover.jpg"
-          },
-
+         {"title":"AllINeedIsSixMillionDollarsToGetIntoNASDAQ", "url":"./sounds/cnbc/AllINeedIsSixMillionDollarsToGetIntoNASDAQ.WAV","background":"./img/cnbc/cover.svg"},
+{"title":"BecauseMyMarketIsAShadowBankingMarket","url":"./sounds/cnbc/BecauseMyMarketIsAShadowBankingMarket.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Because This Is Euphoric Mania", "url":"./sounds/cnbc/BecauseThisIsEuphoricMania.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Bell Floor Ringing", "url":"./sounds/cnbc/BellFloorRinging.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Beta Testing", "url":"./sounds/cnbc/BetaTesting.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Convert That Into Ethereum", "url":"./sounds/cnbc/ConvertThatIntoEthereum.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Cryptocurrencies Are Going To Play A Bigger Role In Microlending", "url":"./sounds/cnbc/CryptocurrenciesAreGoingToPlayABiggerRoleInMicrolending.wav","background":"./img/cnbc/cover.svg"},
+{"title":"ImAShadowBanking Guy", "url":"./sounds/cnbc/ImAShadowBankingGuy.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Im Lending You In Ethereum", "url":"./sounds/cnbc/ImLendingYouInEthereum.wav","background":"./img/cnbc/cover.svg"}
+,
+{"title":"Im Not Listing My Coin Anywhere", "url":"./sounds/cnbc/ImNotListingMyCoinAnywhere.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Insane Market Cap You Know", "url":"./sounds/cnbc/InsaneMarketCapYouKnow.wav","background":"./img/cnbc/cover.svg"}
+,
+{"title":"I Own 55 Percent Of The Company", "url":"./sounds/cnbc/IOwn55PercentOfTheCompany.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Is It Not Genuine", "url":"./sounds/cnbc/IsItNotGenuine.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Microfinancing", "url":"./sounds/cnbc/Microfinancing.wav","background":"./img/cnbc/cover.svg"},
+{"title":"My Share holders", "url":"./sounds/cnbc/MyShareholders.wav","background":"./img/cnbc/cover.svg"},
+{"title":"One Thing", "url":"./sounds/cnbc/OneThing.wav","background":"./img/cnbc/cover.svg"},
+{"title":"On NASDAQ", "url":"./sounds/cnbc/OnNASDAQ.WAV","background":"./img/cnbc/cover.svg"},
+{"title":"reggae", "url":"./sounds/cnbc/reggae.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Shadow Banking Is Seventy Two Trillion Dollars", "url":"./sounds/cnbc/ShadowBankingIsSeventyTwoTrillionDollars.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Use That", "url":"./sounds/cnbc/UseThat.wav","background":"./img/cnbc/cover.svg"},
+{"title":"We Are A Growing Company Well Grow", "url":"./sounds/cnbc/WeAreAGrowingCompanyWellGrow.wav","background":"./img/cnbc/cover.svg"},
+{"title":"We Did A Genuine IPO", "url":"./sounds/cnbc/WeDidAGenuineIPO.WAV","background":"./img/cnbc/cover.svg"},
+{"title":"We Dont Deserve This Market Cap", "url":"./sounds/cnbc/WeDontDeserveThisMarketCap.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Were A Profitable Company", "url":"./sounds/cnbc/WereAProfitableCompany.wav","background":"./img/cnbc/cover.svg"}
+,
+{"title":"Whats Wrong With It", "url":"./sounds/cnbc/WhatsWrongWithIt.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Yes", "url":"./sounds/cnbc/Yes.wav","background":"./img/cnbc/cover.svg"},
+{"title":"Ziddu Coin", "url":"./sounds/cnbc/ZidduCoin.wav","background":"./img/cnbc/cover.svg"}
         ],
       },
 
-      {
-        "title" : 'Les simpsons',
-        "banner" : "./img/simpsons/cover.jpg",
-        "bgcolor": "#616c0b",
-        "sounds" : [
-          {
-            "title" : "Dacodac",
-            "url" : "./sounds/simpsons/dacodac.mp3",
-          },
-          {
-            "title" : "Doh",
-            "url" : "./sounds/simpsons/doh.mp3",
-          },
-          {
-            "title" : "Et bien moi aussi",
-            "url" : "./sounds/simpsons/et-bien-moi-aussi.mp3",
-          },
-
-          {
-            "title" : "Ici homer simpsons",
-            "url" : "./sounds/simpsons/ici-homer-simpsons.mp3",
-          },
-
-          {
-            "title" : "Je vais le dire à la police",
-            "url" : "./sounds/simpsons/je-vais-le-dire-a-la-police.mp3",
-          },
-
-          {
-            "title" : "On se revera au tribunal",
-            "url" : "./sounds/simpsons/on-se-revera-au-tribunal.mp3",
-          },
-          {
-            "title" : "Qui c'est ?",
-            "url" : "./sounds/simpsons/qui-c-est.mp3",
-          },
-          {
-            "title" : "You tried",
-            "url" : "./sounds/simpsons/you tried.mp3",
-          },
-        ],
-      },
-      {
-        "title" : 'Breaking bad',
-        "banner" : "./img/bb/cover.jpg",
-        "bgcolor": "#26a711",
-        "sounds" : [
-          {
-            "title" : "You're god damn right",
-            "url" : "./sounds/bb/You're_god_damn_right.wav",
-          },
-          {
-            "title" : "Don't bullshit a bullshiter",
-            "url" : "./sounds/bb/dbbs.wav",
-          },
-          {
-            "title" : "I'm the danger",
-            "url" : "./sounds/bb/I'm_the_danger.wav",
-          },
-          {
-            "title" : "I'm the one who knocks",
-            "url" : "./sounds/bb/I'm_the_one_who_knocks.wav",
-          },
-          {
-            "title" : "Now say my name",
-            "url" : "./sounds/bb/Now_say_my_name.wav",
-          },
-          {
-            "title" : "Yeah B*tch",
-            "url" : "./sounds/bb/Yeah_Bitch!.wav",
-          },
-          {
-            "title" : "A man provides",
-            "url" : "./sounds/bb/A_man_provides.wav",
-            "background" : "./img/sv/cover.jpg"
-          },
-          {
-            "title" : "Better Call Saul",
-            "url" : "./sounds/bb/Better_Call_Saul.wav",
-            "background" : "./img/sv/cover.jpg"
-          },
-        ]
-      },
-      // {
-      //   "title" : 'The Soprano',
-      //   "banner" : "./img/sopranos/cover.jpg",
-      //   "bgcolor": "#a71111",
-      //   "excerpt" : "Lorem ipsum dolor sit met, qui at desert mandamus, adduce ullum apeirian mea at",
-      //   "content": "lorem lorem Lorem ipsum dolor sit met, qui at desert mandamus, adduce ullum apeirian mea at. Eu mel vide saltando vituperata, sonet quidam deterruisset te qui. Te cum vivendum explicate abhorrent. Id venom argumentum vel. Ut lorem bocent hendrerit eam. orem lorem Lorem ipsum dolor sit met, qui at desert mandamus, adduce ullum apeirian mea at. Eu mel vide saltando vituperata, sonet quidam deterruisset te qui. Te cum vivendum explicate abhorrent. Id venom argumentum vel. Ut lorem bocent hendrerit eam. orem lorem Lorem ipsum dolor sit met, qui at desert mandamus, adduce ullum apeirian mea at. Eu mel vide saltando vituperata, sonet quidam deterruisset te qui. Te cum vivendum explicate abhorrent. Id venom argumentum vel. Ut lorem bocent hendrerit eam. orem lorem Lorem ipsum dolor sit met, qui at desert mandamus, adduce ullum apeirian mea at. Eu mel vide saltando vituperata, sonet quidam deterruisset te qui. Te cum vivendum explicate abhorrent. Id venom argumentum vel. Ut lorem bocent hendrerit eam."
-      // },
+     
     ],
     getArticles :  function(){
       return this.articles;
